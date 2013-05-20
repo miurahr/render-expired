676c28ba717b452aa5c04c43da28f4fa68a8f5b4
Hiroshi Miura <miurahr@linux.com>
2013-05-21 07:53:58 +0900

    remove example debian files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(render-expired):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="render-expired" command="/usr/bin/render-expired"
--- debian/render-expired.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the render-expired package
-#
-0 4	* * *	root	[ -x /usr/bin/render-expired_maintenance ] && /usr/bin/render-expired_maintenance
